f37abbca2cba7f3e91a57f2c1f5f24dba7a092c7
rohit <rohit@yieldmo.com>
2015-03-06 18:18:05 -0500

    Fix formatting issues

--- tsvToDataTable.py
+++ tsvToDataTable.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import sys
 import argparse
 
 def parseArguments():
@@ -23,17 +24,11 @@ def writeHeaders(htmlFile):
 		<link rel='stylesheet' type='text/css' href='http://cdn.datatables.net/plug-ins/f2c75b7247b/integration/jqueryui/dataTables.jqueryui.css'/>
 		<script type='text/javascript'>
 			$(document).ready(function() {
-				var table = $('#myTable').DataTable( {
-				    "scrollY": "300px",
-				    "paging": true
-				} );
-
+				var table = $('#myTable').DataTable();
 				$('a.toggle-vis').on( 'click', function (e) {
 				    e.preventDefault();
-
 				    // Get the column API object
 				    var column = table.column( $(this).attr('data-column') );
-
 				    // Toggle the visibility
 				    column.visible( ! column.visible() );
 				} );
@@ -54,8 +49,8 @@ def writeFooters(htmlFile):
 
 def getFakeColumnNames(fromFile):
 	with open(fromFile) as f:
-		first_line = f.readline().strip()
-		count = len("\t".split(first_line))
+		first_line = f.readline().rstrip('\n')
+		count = len(first_line.split("\t"))
 
 	return ["Column " + str(x) for x in range(1, count + 1) ]
 
@@ -66,27 +61,30 @@ def main():
 
 	if(args.headersFrom) :
 		headerFile = open(args.headersFrom, 'r')
-		headers = headerFile.readline().strip().split(",")
+		headers = headerFile.readline().rstrip('\n').split(",")
 	elif args.colNames:
 		headers = args.colNames
 	elif args.hasHeaders == False :
-		headers = getFakeColumnNames()
+		headers = getFakeColumnNames(args.inputFile)
 
 	with open(args.inputFile) as f:
 		if args.headersFrom == None and args.hasHeaders :
-			headers = f.readline().strip().split("\t")
+			headers = f.readline().rstrip('\n').split("\t")
+		nHeaders = len(headers)
 		htmlFile.write('<div style="text-align:center">Toggle Visibility - ')
 		for idx, header in enumerate(headers):
 			htmlFile.write('<a class="toggle-vis" data-column="{0}" href="#" style="padding: 4px">{1}</a>'.format(idx, header))
 		htmlFile.write('<br></div>')
-		htmlFile.write('''<table id='myTable' class='display' cellspacing="0" width="100%">
+		htmlFile.write('''<table id='myTable' class='display compact' cellspacing="0" width="100%">
 			<thead>''')
 		htmlFile.write('<tr>')
 		for header in headers:
 			htmlFile.write('<th>' + header + '</th>')
 		htmlFile.write('</tr>\n</thead>\n<tbody>\n')
 		for line in f:
-			htmlFile.write('<tr><td>' + line.strip().replace('\t', '</td><td>') + '</td></tr>\n')
+			if nHeaders != len(line.split("\t")):
+				sys.stderr.write("Number of headers did not match number of rows for :" + line) 
+			htmlFile.write('<tr><td>' + line.rstrip('\n').replace('\t', '</td><td>') + '</td></tr>\n')
 		htmlFile.write('</tbody>')
 		htmlFile.write('<tfoot><tr>')
 		for header in headers:
